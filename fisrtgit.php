68cefde539af8299becc0923abb2473db9e45eac
Tamizhazhagansundaram <97866444+Tamizhazhagansundaram@users.noreply.github.com>
2022-01-20 14:05:45 +0530

    variable changed hours into hrs.

--- fisrtgit.php
+++ fisrtgit.php
@@ -5,8 +5,8 @@ $date2 = new DateTime('2006-04-12T10:03:00');
 
 $diff = $date2->diff($date1);
 
-$hours = $diff->h;
-$hours = $hours + ($diff->days*24);
+$hrs = $diff->h;
+$hrs = $hrs + ($diff->days*24);
 
-echo $hours;
+echo $hrs;
 ?>
